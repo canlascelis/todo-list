1d1d1be80d001adbc949c890fd1d88e1c3a30d71
Jeremiah Celis <canlascelis@gmail.com>
2022-01-27 12:45:01 +0800

    1/27

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,11 @@
 // BEGIN today's date
 const addButton = document.getElementById("addBtn");
+const deleteButton = document.getElementById("deleteBtn");
 const date = new Date();
 options = { weekday: "short", month: "short", day: "numeric", year: "numeric" } //options for date 
 
-let today = date.toLocaleDateString("en-US", options);
+let today = date.toLocaleDateString("en-US", options);        //convert date to string
+let userInput = document.getElementById("user-input").value; //input from user html
 console.log(today);
 // END today's date
 
@@ -12,81 +14,22 @@ console.log(today);
 // let savedTodo = localStorage.getItem("Todo");
 // document.getElementById("ulContent").innerHTML = localStorage.getItem("Todo");
 
-// function addTodo() {
-//     let inputVal = document.getElementById("user-input").value;
-//     let ul = document.getElementById("ulContent");
-//     let li = document.createElement("li");
-//     let createText = document.createTextNode(inputVal);
+addButton.addEventListener('click', () => { 
+    let userInput = document.getElementById("user-input").value;
+    let taskList = JSON.parse(localStorage.getItem("Tasks")) || []; //parse or empty array
 
-//     if (inputVal == "") {
-//         alert("Input something")
-//     } else {
-//         li.appendChild(createText);
-//         ul.appendChild(li);
-//     }
+    let tasks = [
+        {
+         todos: userInput,
+         dateCreated: today   
+        }
+    ];
 
-//     console.log(inputVal);
-// }
-
-// function addTodo2() {
-//     let inputVal = document.getElementById("user-input").value;
-//     let id = 0;
-
-//     let tasks = {
-//         id: id,
-//         todo: inputVal,
-//         dateCreated: today
-//     };
-
-//     if (inputVal == "") {
-//         alert("Please type something...");
-//     } else {
-//         // localStorage.setItem("Todo", JSON.stringify(tasks));
-//         localStorage.setItem("Todo", JSON.stringify(tasks));
-//         id++;
-//         // tasks.push(inputVal);
-//     }
-// }
-
-class Storage {
-    constructor (task, id) {
-        this.id = id;
-        this.task = task;
-        this.dateCreated = today;
-    }
-
-    // TODO: create a method that will insert the task dyanmically
-    create(items) {
-        let value = JSON.stringify(items);
-        localStorage.setItem("Todo", value);
-    }
-}
-
-// const storage = new Storage();
-// storage.create;
-// console.log(storage);
-
-// function addTodo2() {
-//     let userInput = document.getElementById("user-input").value;
-//     let id = 0;
-//     const items = new Storage(userInput, id);
-
-//     if (userInput == '') {
-//         alert("Please input something");
-//     } else {
-//         items.create(userInput);
-//         console.log(items);
-//     }
-// }
-
-addButton.addEventListener('click', () => {
-    let userInput = document.getElementById("user-input").value; //input from user html
-    let id = 0;
-    const storage = new Storage(userInput, id);
-   
     if (userInput == '') {
-        alert("Please write something!")
+        alert("Please write something");
     } else {
-        console.log("storage");
+        taskList.push(tasks);                                     //push tasks object 
+        localStorage.setItem("Tasks", JSON.stringify(taskList)); //setitem in LS into JSON object
+        alert("Successfully added!")
     }
 });
