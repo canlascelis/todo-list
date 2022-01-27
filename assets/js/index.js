5190c2ee877660b023aac4a8c04392cb5412e347
Jeremiah Celis <canlascelis@gmail.com>
2022-01-27 16:01:38 +0800

    1/27/22 = using classes

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,26 +10,57 @@ console.log(today);
 // END today's date
 
 //RETRIEVE item (local storage)
-// document.getElementById("li-one").innerHTML = localStorage.getItem("lastname")
-// let savedTodo = localStorage.getItem("Todo");
-// document.getElementById("ulContent").innerHTML = localStorage.getItem("Todo");
+//document.getElementById("li-one").innerHTML = localStorage.getItem("Todo")
+//let savedTodo = localStorage.getItem("Todo");
+//document.getElementById("ulContent").innerHTML = localStorage.getItem("Todo");
+
+// addButton.addEventListener('click', () => { 
+//     let userInput = document.getElementById("user-input").value;
+//     let taskList = JSON.parse(localStorage.getItem("Tasks")) || []; //parse or empty array
+
+//     let tasks = [
+//         {
+//          todos: userInput,
+//          dateCreated: today   
+//         }
+//     ];
+
+//     if (userInput == '') {
+//         alert("Please write something");
+//     } else {
+//         taskList.push(tasks);                                     //push tasks object 
+//         localStorage.setItem("Tasks", JSON.stringify(taskList)); //setitem in LS into JSON object
+//         alert("Successfully added!")
+//     }
+// });
+
+// let tasks = {
+//         todos: userInput,
+//         dateCreated: today   
+//     };
+
+class Storage {
+    constructor(){
+        let userInput = document.getElementById("user-input").value;
+        this.todo = userInput;
+        this.dateCreated = today;
+    }
+
+    create(){
+        let store = new Storage();
+        let taskList = JSON.parse(localStorage.getItem("Tasks")) || [];
+        taskList.push(store);
+        localStorage.setItem("Tasks", JSON.stringify(taskList));
+    }
+}
 
 addButton.addEventListener('click', () => { 
     let userInput = document.getElementById("user-input").value;
-    let taskList = JSON.parse(localStorage.getItem("Tasks")) || []; //parse or empty array
+    let storage = new Storage();
 
-    let tasks = [
-        {
-         todos: userInput,
-         dateCreated: today   
-        }
-    ];
-
-    if (userInput == '') {
-        alert("Please write something");
-    } else {
-        taskList.push(tasks);                                     //push tasks object 
-        localStorage.setItem("Tasks", JSON.stringify(taskList)); //setitem in LS into JSON object
-        alert("Successfully added!")
-    }
+   if (userInput == '') {
+       alert("Please write something!")
+   } else {
+    storage.create();
+   }
 });
